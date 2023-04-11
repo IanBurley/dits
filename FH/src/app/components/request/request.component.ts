f5da23b699be5bc08685cd99404b1b9d1aceeb4e
omarch21 <omarch641@gmail.com>
2023-04-11 00:30:45 -0500

    adding db

--- FH/src/app/components/request/request.component.ts
+++ FH/src/app/components/request/request.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap} from 'rxjs'
 import { FormGroup,FormControl,Validators } from '@angular/forms';
 
+import { DonationRequest } from 'src/app/models/request.module';
+import {uuidv4} from '@firebase/util'
+import { UsersService } from 'src/services/users.service';
 interface RequestType{
   type: string
 }
@@ -9,14 +14,21 @@ interface RequestType{
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.css']
 })
-export class RequestComponent {
+export class RequestComponent implements OnInit{
+  user$ = this.usersService.currentUserProfile$;
+  constructor(private usersService: UsersService,private router: Router){
 
+  }
+request: DonationRequest = {type: '',notes: '',id:'',dateinserted:''};
   requestForm = new FormGroup({
-    type: new FormControl<RequestType | null>(null,Validators.required),
-    notes: new FormControl('',Validators.required)
+    requesttype: new FormControl<string | null>(null,Validators.required),
+    requestnotes: new FormControl('',Validators.required)
 
 
   })
+  ngOnInit(): void{
+   
+  }
   types: RequestType[] = [
     {type: 'Furniture'},
     {type: 'Canned Goods'},
@@ -25,4 +37,29 @@ export class RequestComponent {
     {type: 'Electronics'},
     {type: 'Other'}
   ]
+
+  clickSubmit(){
+    if(!this.requestForm.valid){
+      return;
+    }
+    const type = this.requestForm.get('requesttype')?.value!;
+    const notes = this.requestForm.get('requestnotes')?.value ?? '';
+    const id = uuidv4();
+    const date1 = new Date().toISOString();
+    this.request.type = type;
+    this.request.notes = notes;
+    this.request.id = id;
+    this.request.dateinserted = date1;
+    console.log(this.request);
+    this.user$.pipe(tap((user)=>{
+
+      if(user){
+        console.log(user?.uid);
+
+      this.usersService.addRequest(this.request,user?.uid)
+      }
+    })).subscribe();
+    this.router.navigate(['/home']);
+  }
+
 }
